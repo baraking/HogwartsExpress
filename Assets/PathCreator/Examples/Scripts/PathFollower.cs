82f0b5e23c056a47fb5b85a26879ece8972a970a
baraking <barakatz.bk@gmail.com>
2020-10-25 23:25:27 +0200

    added speed up and down near stations.

--- Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -6,14 +6,19 @@ namespace PathCreation.Examples
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
     public class PathFollower : MonoBehaviour
     {
+        public static readonly float OriginalSpeed = 15;
+
         public PathCreator pathCreator;
         public EndOfPathInstruction endOfPathInstruction;
-        public float speed = 5;
+        public float speed = OriginalSpeed;
         public Vector3 originPosition;
-        float distanceTravelled;
+        public float distanceTravelled;
 
         public static Vector3 heightOfCart = new Vector3(0, 0.5f, 0);
         public static float initialRotation = 90;
+
+        public float distanceFromStation = 0.95f;
+        public float distanceFromStationThreshold = 0.15f;
 
         private static readonly string fullTrain = "Train";
         private static readonly string trainCart = "Cart";
@@ -30,14 +35,12 @@ namespace PathCreation.Examples
         }
 
         void Start() {
-            originPosition=transform.localPosition;
-            if(pathCreator == null)
+            originPosition = transform.localPosition;
+            if (pathCreator == null)
             {
                 if (transform.parent != null && transform.parent.tag == fullTrain)
                 {
-                    pathCreator = transform.parent.GetComponentInParent<PathFollower>().pathCreator;
-                    speed = transform.parent.GetComponentInParent<PathFollower>().speed;
-                    endOfPathInstruction = transform.parent.GetComponentInParent<PathFollower>().endOfPathInstruction;
+                    UpdateChildCart();
                 }
             }
             if (pathCreator != null)
@@ -52,16 +55,96 @@ namespace PathCreation.Examples
             if (pathCreator != null)
             {
                 distanceTravelled += speed * Time.deltaTime;
-                transform.position = heightOfCart + pathCreator.path.GetPointAtDistance(distanceTravelled - originPosition.magnitude, endOfPathInstruction);
+                float relatedLocationOnTrack = getRealtedLocationOnTrack();
+                if(transform.tag == fullTrain)
+                {
+                    if(relatedLocationOnTrack < 1- distanceFromStation)
+                    {
+                        TrainSetSpeed(OriginalSpeed / 3);
+                    }
+                    else if (relatedLocationOnTrack > 1 - distanceFromStation&&relatedLocationOnTrack < 1 - distanceFromStation + distanceFromStationThreshold)
+                    {
+                        TrainAccelerateUp(relatedLocationOnTrack, OriginalSpeed / 3, OriginalSpeed);
+                    }
+                    else if(relatedLocationOnTrack > distanceFromStation - distanceFromStationThreshold && relatedLocationOnTrack < distanceFromStation)
+                    {
+                        TrainAccelerateDown(relatedLocationOnTrack, OriginalSpeed,OriginalSpeed / 3);
+                    }
+                    else if (relatedLocationOnTrack > distanceFromStation && relatedLocationOnTrack < 1)
+                    {
+                        TrainSetSpeed(OriginalSpeed / 3);
+                    }
+                    else if (relatedLocationOnTrack >= 1)
+                    {
+                        TrainStop();
+                    }
+                    else if (speed != OriginalSpeed)
+                    {
+                        TrainSetSpeed(OriginalSpeed);
+                    }
+                }
+
+                transform.position = heightOfCart + pathCreator.path.GetPointAtDistance(distanceTravelled - originPosition.magnitude * 0.75f, endOfPathInstruction);
                 transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled - originPosition.magnitude, endOfPathInstruction);
                 transform.rotation = Quaternion.AngleAxis(90, transform.forward) * transform.rotation;
             }
+        }
+
+        void TrainAccelerateDown(float relatedLocationOnTrack,float originSpeed ,float targetSpeed)
+        {
+            float minValue = distanceFromStation - distanceFromStationThreshold;
+            float speedVar = Mathf.Lerp(0, 1, (relatedLocationOnTrack - minValue) * (1 / distanceFromStationThreshold));
+            speed = (originSpeed - (speedVar * (originSpeed - (targetSpeed))));
+            UpdateSpeedForChildCarts(speed);
+        }
+
+        void TrainAccelerateUp(float relatedLocationOnTrack, float originSpeed, float targetSpeed)
+        {
+            float minValue = distanceFromStation - distanceFromStationThreshold;
+            float speedVar = Mathf.Lerp(0, 1, (relatedLocationOnTrack - (1 - distanceFromStation)) * (1 / distanceFromStationThreshold));
+            print(speedVar);
+            speed = (originSpeed + (speedVar * (targetSpeed - originSpeed)));
+            UpdateSpeedForChildCarts(speed);
+        }
+
+        void TrainStop()
+        {
+            speed = 0;
+            UpdateSpeedForChildCarts(speed);
+        }
+
+        void TrainSetSpeed(float newSpeed)
+        {
+            speed = newSpeed;
+            UpdateSpeedForChildCarts(speed);
+        }
+
+        void UpdateSpeedForChildCarts(float newSpeed)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                if (child.gameObject.CompareTag(trainCart))
+                {
+                    child.GetComponent<PathFollower>().speed = newSpeed;
+                }
+            }
+        }
+
+        void UpdateChildCart(){
+            pathCreator = transform.parent.GetComponentInParent<PathFollower>().pathCreator;
+            speed = transform.parent.GetComponentInParent<PathFollower>().speed;
+            endOfPathInstruction = transform.parent.GetComponentInParent<PathFollower>().endOfPathInstruction;
         }
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
         // is as close as possible to its position on the old path
         void OnPathChanged() {
             distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+
+        float getRealtedLocationOnTrack()
+        {
+            return ((distanceTravelled - originPosition.magnitude * 0.75f) / pathCreator.path.length);
         }
     }
 }
