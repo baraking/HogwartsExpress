4b24e80f71151c21b92cafda53f7d30a21d201e5
baraking <barakatz.bk@gmail.com>
2020-10-30 16:37:54 +0200

    triggers work, need to fix waitForSeconds and Accelerate

--- Assets/PathCreator/Core/Runtime/PathFollower.cs
+++ Assets/PathCreator/Core/Runtime/PathFollower.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace PathCreation.Examples
+namespace PathCreation
 {
     // Moves along a path at constant speed.
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
@@ -19,6 +19,8 @@ namespace PathCreation.Examples
 
         public float distanceFromStation = 0.95f;
         public float distanceFromStationThreshold = 0.15f;
+
+        public static float accelarationSpeed = 10f;
 
         public int trainSize = 0;
         public float sizeOfCart = 4;
@@ -61,112 +63,44 @@ namespace PathCreation.Examples
             if (pathCreator != null)
             {
                 distanceTravelled += speed * Time.deltaTime;
-                float relatedLocationOnTrack = GetRealtedLocationOnTrack();
-                float extraSpace = 0f;
-                if (transform.tag == Constants.fullTrain)
-                {
-                    if (endOfPathInstruction != previousEndOfPathInstruction)
-                    {
-                        previousEndOfPathInstruction = endOfPathInstruction;
-                        UpdateAllChildrenData();
-                    }
-
-                    if (endOfPathInstruction == EndOfPathInstruction.Loop)
-                    {
-                        if (relatedLocationOnTrack >= 1)
-                        {
-                            while (GetRealtedLocationOnTrack() >= 1)
-                            {
-                                distanceTravelled -= distanceTravelled - originPosition.magnitude * 0.75f;
-                                UpdateAllChildrenDistanceTravelled();
-                            }
-                            relatedLocationOnTrack = GetRealtedLocationOnTrack();
-                        }
-                    }
-                    else if (endOfPathInstruction == EndOfPathInstruction.Stop)
-                    {
-                        if (relatedLocationOnTrack > 1.1)
-                        {
-                            while (GetRealtedLocationOnTrack() >= 1.1)
-                            {
-                                distanceTravelled -= distanceTravelled - originPosition.magnitude * 0.75f;
-                                UpdateAllChildrenDistanceTravelled();
-                            }
-                            relatedLocationOnTrack = GetRealtedLocationOnTrack();
-                        }
-                    }
-
-
-                    if (relatedLocationOnTrack < 1- distanceFromStation)
-                    {
-                        TrainSetSpeed(Constants.OriginalSpeed / 3);
-                    }
-                    else if (relatedLocationOnTrack > 1 - distanceFromStation&&relatedLocationOnTrack < 1 - distanceFromStation + distanceFromStationThreshold)
-                    {
-                        TrainAccelerateUp(relatedLocationOnTrack, Constants.OriginalSpeed / 3, Constants.OriginalSpeed);
-                    }
-                    else if(relatedLocationOnTrack > distanceFromStation - distanceFromStationThreshold && relatedLocationOnTrack < distanceFromStation)
-                    {
-                        TrainAccelerateDown(relatedLocationOnTrack, Constants.OriginalSpeed, Constants.OriginalSpeed / 3);
-                    }
-                    else if (relatedLocationOnTrack > distanceFromStation && relatedLocationOnTrack < 1)
-                    {
-                        TrainSetSpeed(Constants.OriginalSpeed / 3);
-                    }
-                    else if (relatedLocationOnTrack >= 1)
-                    {
-                        if(endOfPathInstruction == EndOfPathInstruction.Stop)
-                        {
-                            TrainStop();
-                        }
-                        else if(endOfPathInstruction == EndOfPathInstruction.Loop)
-                        {
-                            distanceTravelled = extraSpace;
-                            UpdateAllChildrenData();
-                        }
-                        else if (endOfPathInstruction == EndOfPathInstruction.Reverse)
-                        {
-                            //Need To Add Here!!!
-                            UpdateAllChildrenData();
-                            TrainSetSpeed(Constants.OriginalSpeed / 3);
-                        }
-                    }
-                    else if (speed != Constants.OriginalSpeed)
-                    {
-                        TrainSetSpeed(Constants.OriginalSpeed);
-                    }
-                }
-                print(distanceTravelled - originPosition.magnitude * 0.75f);
                 transform.position = heightOfCart + pathCreator.path.GetPointAtDistance(distanceTravelled - originPosition.magnitude * 0.75f, endOfPathInstruction);
                 transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled - originPosition.magnitude * 0.75f, endOfPathInstruction);
                 transform.rotation = Quaternion.AngleAxis(90, transform.forward) * transform.rotation;
             }
         }
 
-        void TrainAccelerateDown(float relatedLocationOnTrack,float originSpeed ,float targetSpeed)
+        public void TrainAccelerate(float targetSpeed)
         {
-            float minValue = distanceFromStation - distanceFromStationThreshold;
-            float speedVar = Mathf.Lerp(0, 1, (relatedLocationOnTrack - minValue) * (1 / distanceFromStationThreshold));
-            speed = (originSpeed - (speedVar * (originSpeed - (targetSpeed))));
+
+            StartCoroutine(ChangeSpeed(targetSpeed));
+        }
+
+        IEnumerator ChangeSpeed(float targetSpeed)
+        {
+            float duration = Mathf.Abs(speed - targetSpeed) / accelarationSpeed;
+            float elapsed = 0.0f;
+            while (elapsed < duration)
+            {
+                if (speed > targetSpeed)
+                {
+                    speed = Mathf.Lerp(speed, targetSpeed, elapsed / duration);
+                }
+                else
+                {
+                    speed = Mathf.Lerp(targetSpeed, speed, elapsed / duration);
+                }
+                UpdateSpeedForChildCarts(speed);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+            speed = targetSpeed;
             UpdateSpeedForChildCarts(speed);
         }
 
-        void TrainAccelerateUp(float relatedLocationOnTrack, float originSpeed, float targetSpeed)
-        {
-            float minValue = distanceFromStation - distanceFromStationThreshold;
-            float speedVar = Mathf.Lerp(0, 1, (relatedLocationOnTrack - (1 - distanceFromStation)) * (1 / distanceFromStationThreshold));
-            print(speedVar);
-            speed = (originSpeed + (speedVar * (targetSpeed - originSpeed)));
-            UpdateSpeedForChildCarts(speed);
-        }
-
-        void TrainStop()
+        public void TrainStop()
         {
             speed = 0;
             UpdateSpeedForChildCarts(speed);
-            //StartCoroutine(WaitFunction(3));
-            //speed = OriginalSpeed/3;
-            //UpdateSpeedForChildCarts(speed);
         }
 
         public void TrainSetSpeed(float newSpeed)
@@ -229,11 +163,6 @@ namespace PathCreation.Examples
         float GetRealtedLocationOnTrack()
         {
             return ((distanceTravelled - originPosition.magnitude * 0.75f) / pathCreator.path.length);
-        }
-
-        public IEnumerator WaitFunction(int time)
-        {
-            yield return new WaitForSeconds(time);
         }
     }
 }
--- Assets/PathCreator/Core/Runtime/TrainSpeedEvent.cs
+++ Assets/PathCreator/Core/Runtime/TrainSpeedEvent.cs
@@ -4,19 +4,75 @@ using UnityEngine;
 
 public class TrainSpeedEvent : MonoBehaviour
 {
+    public enum SpeedEventMode { set, accelerate, stop };
+
+    public SpeedEventMode mode;
     public float speed;
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
+        if(mode== SpeedEventMode.set)
+        {
+            Gizmos.color = Color.green;
+        }
+        else if (mode == SpeedEventMode.stop)
+        {
+            Gizmos.color = Color.red;
+        }
+        else if (mode == SpeedEventMode.accelerate)
+        {
+            Gizmos.color = Color.blue;
+        }
+
         Gizmos.DrawSphere(transform.position, 1);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == Constants.fullTrain)
+        if (other.tag == Constants.trainCart || other.tag == Constants.fullTrain)
         {
-            
+            if (mode == SpeedEventMode.set)
+            {
+                if (other.gameObject.transform.parent.tag == Constants.fullTrain)
+                {
+                    other.gameObject.transform.parent.GetComponent<PathCreation.PathFollower>().TrainSetSpeed(speed);
+                }
+                else if (other.tag == Constants.fullTrain)
+                {
+                    other.GetComponent<PathCreation.PathFollower>().TrainSetSpeed(speed);
+                }
+            }
+            else if (mode == SpeedEventMode.stop)
+            {
+                if (other.gameObject.transform.parent.tag == Constants.fullTrain)
+                {
+                    other.gameObject.transform.parent.GetComponent<PathCreation.PathFollower>().TrainStop();
+                    StartCoroutine(WaitFunction(Constants.TrainStopWaittime));
+                    other.gameObject.transform.parent.GetComponent<PathCreation.PathFollower>().TrainAccelerate(speed);
+                }
+                else if (other.tag == Constants.fullTrain)
+                {
+                    other.GetComponent<PathCreation.PathFollower>().TrainStop();
+                    StartCoroutine(WaitFunction(Constants.TrainStopWaittime));
+                    other.GetComponent<PathCreation.PathFollower>().TrainAccelerate(speed);
+                }
+            }
+            else if (mode == SpeedEventMode.accelerate)
+            {
+                if (other.gameObject.transform.parent.tag == Constants.fullTrain)
+                {
+                    other.gameObject.transform.parent.GetComponent<PathCreation.PathFollower>().TrainAccelerate(speed);
+                }
+                else if (other.tag == Constants.fullTrain)
+                {
+                    other.GetComponent<PathCreation.PathFollower>().TrainAccelerate(speed);
+                }
+            }
         }
+    }
+
+    public IEnumerator WaitFunction(int time)
+    {
+        yield return new WaitForSecondsRealtime(time);
     }
 }
