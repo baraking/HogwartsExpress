9975bb155c83adee2d9ea58717f4985b8302a13e
baraking <barakatz.bk@gmail.com>
2020-11-13 12:58:05 +0200

    added wizards anv nva. need to fix it and train animation

---
+++ Assets/Prefabs/Wizards/WizardModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardModel : MonoBehaviour
+{
+
+    public GameObject hat;
+    public GameObject scarf;
+    public GameObject head;
+
+    public Material[] skins;
+    public Material[] colors;
+
+    void Start()
+    {
+        int randomColor = Random.Range(0, colors.Length);
+        hat.GetComponent<Renderer>().material = colors[randomColor];
+        scarf.GetComponent<Renderer>().material = colors[randomColor];
+
+        int skinColor = Random.Range(0, skins.Length);
+        head.GetComponent<Renderer>().material = skins[randomColor];
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Prefabs/Wizards/WizardMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WizardMovement : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    public GameObject possibleTargets;
+    Vector3 curTarget;
+
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(gameObject.transform.position, curTarget);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                curTarget = hit.point;
+                navMeshAgent.SetDestination(curTarget);
+            }
+        }
+        else if (distanceToTarget < 3 || curTarget == Vector3.zero)
+        {
+            FindNewTarget();
+        }
+    }
+
+    public void FindNewTarget()
+    {
+        int random = Random.Range(0, possibleTargets.transform.childCount - 1);
+        Vector3 nextTarget = possibleTargets.transform.GetChild(random).position;
+        curTarget = nextTarget;
+        navMeshAgent.SetDestination(curTarget);
+    }
+}
