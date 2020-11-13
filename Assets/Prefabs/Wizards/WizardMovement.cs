debba3998d3b305b1c39e69925055177e83450c6
baraking <barakatz.bk@gmail.com>
2020-11-13 17:04:31 +0200

    moving works

--- Assets/Prefabs/Wizards/WizardMovement.cs
+++ Assets/Prefabs/Wizards/WizardMovement.cs
@@ -8,11 +8,17 @@ public class WizardMovement : MonoBehaviour
     public NavMeshAgent navMeshAgent;
 
     public GameObject possibleTargets;
-    Vector3 curTarget;
+    public GameObject curTarget;
+    //Vector3 curTarget;
+
+    private void Start()
+    {
+
+    }
 
     void Update()
     {
-        float distanceToTarget = Vector3.Distance(gameObject.transform.position, curTarget);
+        /*float distanceToTarget = Vector3.Distance(gameObject.transform.position, curTarget);
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mouse = Input.mousePosition;
@@ -27,14 +33,20 @@ public class WizardMovement : MonoBehaviour
         else if (distanceToTarget < 3 || curTarget == Vector3.zero)
         {
             FindNewTarget();
-        }
+        }*/
+
+        print("I should start");
+        navMeshAgent.SetDestination(curTarget.transform.position);
+        navMeshAgent.destination = curTarget.transform.position;
+
+        print(navMeshAgent.destination);
     }
 
-    public void FindNewTarget()
+    /*public void FindNewTarget()
     {
         int random = Random.Range(0, possibleTargets.transform.childCount - 1);
         Vector3 nextTarget = possibleTargets.transform.GetChild(random).position;
         curTarget = nextTarget;
         navMeshAgent.SetDestination(curTarget);
-    }
+    }*/
 }
